a78cf6026c8b43ab5caa155d4c9a855e166ee943
obashanova <oxana.bashanova@gmail.com>
2020-06-21 16:54:43 +0200

    Updates

---
+++ precache-manifest.459160f1a187198afc8ff4e94163f994.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb00f3eb96d3aa937a4a00d3d4488124",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "eff5dff6c148d8dd01c0",
+    "url": "/robofriends/static/css/main.2b499899.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff5dff6c148d8dd01c0",
+    "url": "/robofriends/static/js/main.c5fc438b.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
